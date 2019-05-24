bcdd673ad49e305e121adb8d6b98d0693f37ce01
Vinicius Menezes <viniciusmenezes@Viniciuss-MacBook-Pro.local>
2019-05-24 12:03:11 -0300

    first commit

---
+++ src/main/java/com/starwars/b2w/API/StarWarsApiApplication.java
@@ -0,0 +1,13 @@
+package com.starwars.b2w.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsApiApplication.class, args);
+	}
+
+}
